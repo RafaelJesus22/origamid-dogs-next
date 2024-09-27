8a8917af82247d7de499ff88ab4a0a9e96407ae0
Rafael Jesus <im.13.45.rafael@gmail.com>
2024-09-27 16:36:24 -0300

    feat: adiciona componente header

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 
 import { type_second } from "@/functions/fonts";
 import "./globals.css";
+import Header from "@/components/header";
 
 export const metadata: Metadata = {
   title: "Dogs Next",
@@ -15,6 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <Header />
       <body className={`${type_second.variable}`}>{children}</body>
     </html>
   );
