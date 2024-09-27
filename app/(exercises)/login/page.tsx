ffa638e807c349246c4f2765f450179dc5ae6fb3
Rafael Jesus <im.13.45.rafael@gmail.com>
2024-09-27 10:31:43 -0300

    feat: cleaning to start final project

--- app/(exercises)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "./login-form";
-
-export default async function Page() {
-  return (
-    <main className="px-4">
-      <LoginForm />
-    </main>
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,60 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
-  const exercises = [
-    {
-      id: 0,
-      name: "Imc",
-      link: "/imc",
-    },
-    {
-      id: 1,
-      name: "Cursos",
-      link: "/cursos",
-    },
-    {
-      id: 2,
-      name: "Login e perfil",
-      link: "/login",
-    },
-    {
-      id: 4,
-      name: "Cache",
-      link: "/produtos",
-    },
-    {
-      id: 5,
-      name: "Image",
-      link: "/animais",
-    },
-  ];
-
   return (
     <main className="max-w-4xl mx-auto p-4">
-      <header>
-        <h1 className="text-2xl font-bold">
-          Exercícios realizados no curso da{" "}
-          <Link
-            href="https://www.origamid.com/"
-            className="text-[#7B3DE9] hover:text-[#6937c1] transition-colors"
-          >
-            Origamid
-          </Link>
-        </h1>
-      </header>
-
-      <section className="mt-8 flex gap-4">
-        {exercises.map((exercise) => (
-          <div
-            key={exercise.id}
-            className="bg-[#bb99f4] p-2 px-6 rounded transition-colors cursor-pointer text-slate-950 hover:text-teal-50  hover:bg-[#7B3DE9]"
-          >
-            <Link href={exercise.link} className="font-bold">
-              {exercise.name}
-            </Link>
-          </div>
-        ))}
-      </section>
+      <h1>Home</h1>
     </main>
   );
 }
