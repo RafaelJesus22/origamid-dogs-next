2fa13d14a23daf86d183835568754185cc670a04
Rafael Jesus <im.13.45.rafael@gmail.com>
2024-10-21 20:58:31 -0300

    feat: página minha conta

--- app/conta/page.tsx
+++ app/conta/page.tsx
@@ -1,13 +1,29 @@
+import getPhotos from "@/actions/get-photos";
+import { getUser } from "@/actions/get-user";
+import Feed from "@/components/custom/feed/feed";
+import Link from "next/link";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "Minha conta",
 };
 
-export default function ContaPage() {
+export default async function ContaPage() {
+  const { data: user } = await getUser();
+  const { data: photos } = await getPhotos({ user: user?.nome });
+
   return (
     <main>
-      <h1>Conta</h1>
+      {photos?.length ? (
+        <Feed photos={photos || []} />
+      ) : (
+        <div>
+          <p className="text-slate-700 text-xl mb-4">Nenhuma foto encontrada</p>
+          <Link href="/conta/postar" className="button inline-block">
+            Poste uma agora
+          </Link>
+        </div>
+      )}
     </main>
   );
 }
