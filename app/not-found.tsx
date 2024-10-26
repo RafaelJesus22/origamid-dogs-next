e4b4141791687a998386a9cba213fb08a249677a
Rafael Jesus <im.13.45.rafael@gmail.com>
2024-10-26 19:45:29 -0300

    feat: erro global e not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,16 @@
-import { HomeIcon } from "lucide-react";
 import Link from "next/link";
 
-export default async function NotFound() {
+export default function NotFound() {
   return (
-    <main className="max-w-4xl mx-auto p-4 flex flex-col items-center justify-center h-[100vh]">
-      <h1 className="font-bold text-3xl mb-4">Pagina não encontrada</h1>
-      <Link href="/" className="flex items-center gap-2 mb-2">
-        <HomeIcon /> Go home
+    <section className="container">
+      <h1 className="title">Página não encontrada</h1>
+      <Link
+        className="button"
+        style={{ marginBottom: "1rem", display: "inline-block" }}
+        href={"/"}
+      >
+        Volte para a Home.
       </Link>
-    </main>
+    </section>
   );
 }
